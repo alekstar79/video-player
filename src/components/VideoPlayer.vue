fbf49e9a137f7b995f1abba0e8b708d80a14f89f
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-03-07 18:25:03 +0400

    update-1.4.9: prepare

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,33 @@
+<template></template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, watch } from 'vue'
+
+interface Props {
+
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emit = defineEmits<{
+  (e: 'click', data): void;
+}>()
+
+let menuElement: HTMLElement | null = null
+
+onMounted(() => {
+  // do something...
+})
+
+onUnmounted(() => {
+  // do something...
+})
+
+watch(() => [
+
+], () => {
+
+}, { deep: true })
+
+defineExpose({})
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,17 @@
 import { extname, resolve } from 'path'
 import { defineConfig } from 'vite'
 
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   root: '.',
   publicDir: 'public',
   resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
+    alias: { '@': resolve(__dirname, 'src') },
+    extensions: ['.ts', '.vue']
   },
   plugins: [
-    // {
-    //   name: 'configure-response-headers',
-    //   configureServer: (server) => {
-    //     server.middlewares.use((req, res, next) => {
-    //       if (req.url && /\.(mp4|webm|ogg|png|ico|svg|jpe?g)$/.test(req.url)) {
-    //         res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
-    //
-    //         if (req.url.includes('favicon')) {
-    //           res.setHeader('Content-Type', 'image/x-icon')
-    //         }
-    //       }
-    //
-    //       next()
-    //     })
-    //   }
-    // }
+    vue()
   ],
   build: {
     outDir: 'dist',
