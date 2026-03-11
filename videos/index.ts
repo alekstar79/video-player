51ebbfff8957567b2821e4a0c0c24ec23d2a6b71
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-03-11 14:27:26 +0400

    correct

--- videos/index.ts
+++ videos/index.ts
@@ -1,8 +1,8 @@
-// export const getVideoUrl = (filename: string) => new URL(filename, import.meta.url).href
+export const getVideoUrl = (filename: string) => new URL(filename, import.meta.url).href
 
 export default [
-  // getVideoUrl('/Synthwave.mp4'),
-  // getVideoUrl('/GalaxyNebula.mp4'),
+  getVideoUrl('/Synthwave.mp4'),
+  getVideoUrl('/GalaxyNebula.mp4'),
   {
     description: 'Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain\'t no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttps://www.bigbuckbunny.org',
     source: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
